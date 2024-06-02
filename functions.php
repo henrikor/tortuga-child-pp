a6758a0dc2d4b60f0fdc2bf84335501a373f6f53
Henrik Ormåsen <henrik.ormasen@gmail.com>
2024-06-02 16:44:18 +0200

    First commit

---
+++ functions.php
@@ -0,0 +1,51 @@
+<?php
+
+function tortuga_child_enqueue_styles() {
+    wp_enqueue_style('parent-style', get_template_directory_uri() . '/style.css');
+}
+add_action('wp_enqueue_scripts', 'tortuga_child_enqueue_styles');
+
+function filter_frontpage_category($query) {
+    if ($query->is_home() && $query->is_main_query()) {
+        $query->set('cat', '19');  // Bruker kategori-ID 19
+    }
+}
+add_action('pre_get_posts', 'filter_frontpage_category');
+
+function add_epub_link_to_meta() {
+    if (is_single() && function_exists('display_epub_link')) {
+        error_log("display_epub_link exists");
+
+        $dom = new DOMDocument();
+        $dom->preserveWhiteSpace = false;
+        $dom->formatOutput = true;
+
+        // Define XML namespace for xlink
+        $dom->loadXML('<?xml version="1.0" encoding="UTF-8"?><root xmlns:xlink="http://www.w3.org/1999/xlink"></root>');
+
+        @$dom->loadHTML('<?xml encoding="utf-8" ?>' . ob_get_clean());
+
+        $xpath = new DOMXPath($dom);
+        $entry_meta = $xpath->query('//div[contains(@class, "entry-meta")]')->item(0);
+
+        if ($entry_meta) {
+            $link = display_epub_link();
+
+            $fragment = $dom->createDocumentFragment();
+            // Add XML namespace for xlink
+            $fragment->appendXML('<div xmlns:xlink="http://www.w3.org/1999/xlink">' . $link . '</div>');
+            $entry_meta->appendChild($fragment);
+
+            echo $dom->saveHTML();
+            error_log('EPUB link inserted into entry-meta.');
+        } else {
+            error_log('Entry-meta div not found.');
+        }
+    } else {
+        error_log('display_epub_link function not found.');
+    }
+}
+
+add_action('wp_footer', 'add_epub_link_to_meta', 20);
+
+?>
